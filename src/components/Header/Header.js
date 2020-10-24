43480e5c71cf36ae56cbc4dccab21b2bfcef0aff
Riana <rianashahid@gmail.com>
2020-10-23 23:56:07 -0400

    started header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+// import {Link} from 'react-router-dom'
+// import logo -- once the logo is made, we can just put it into the nav bar
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div>
+                {/* <Link to="/"> */}
+                    <p>LOGO</p>
+                {/* </Link> */}
+            </div>
+            <div className="">
+
+            </div>
+        </div>
+    )
+}
