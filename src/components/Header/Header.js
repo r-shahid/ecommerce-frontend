93aa7fb3e2b1c1b90e99de0054e4db80181b4a67
Riana <rianashahid@gmail.com>
2020-10-24 00:14:08 -0400

    header ready for links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,35 @@
 import React from 'react'
+import './Header.css';
 // import {Link} from 'react-router-dom'
 // import logo -- once the logo is made, we can just put it into the nav bar
 
 const Header = () => {
     return (
         <div className="header">
-            <div>
+            <div className="logo">
                 {/* <Link to="/"> */}
                     <p>LOGO</p>
                 {/* </Link> */}
             </div>
-            <div className="">
-
+            <div className="page-links">
+                <div>
+                    {/* <Link to="/about"> */}
+                    <p>About</p>
+                    {/* </Link>  */}
+                </div>
+                <div>
+                    {/* <Link to="/about"> */}
+                    <p>Cart</p>
+                    {/* </Link>  */}
+                </div>
+                <div>
+                    {/* <Link to="/about"> */}
+                    <p>Sell</p>
+                    {/* </Link>  */}
+                </div>
             </div>
         </div>
     )
 }
+
+export default Header
