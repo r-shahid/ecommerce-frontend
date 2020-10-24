a97a952657a502779a7091da15b57819fba35ec2
Riana <rianashahid@gmail.com>
2020-10-24 00:38:39 -0400

    static cart component is fully done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,50 @@
 import React from 'react';
 import './Cart.css'
+import placeholder from "../images/placeholder.jpg"
+
 const Cart = (props) => {
-	console.log('props-Cart', props);
+	// console.log('props-Cart', props);
 	return (
-		<div>
-			<h1>Cart</h1> 
+		<div className='cart-content'>
+			<h1>Cart</h1>
+			{/* later these items will be pulled from whatever is in the cart --> same way that favorites is handled */}
+			<div className='each-product'>
+				<div className='product-info'>
+					<p>PRODUCT NAME</p>
+					<p>$100</p>
+				</div>
+				<div>
+					<img src={placeholder} />
+				</div>
+			</div>
+			<div className='each-product'>
+				<div className='product-info'>
+					<p>PRODUCT NAME</p>
+					<p>$100</p>
+				</div>
+				<div>
+					<img src={placeholder} />
+				</div>
+			</div>
+			<div className='each-product'>
+				<div className='product-info'>
+					<p>PRODUCT NAME</p>
+					<p>$100</p>
+				</div>
+				<div>
+					<img src={placeholder} />
+				</div>
+			</div>
+			<div className='each-product'>
+				<div className='product-info'>
+					<p>PRODUCT NAME</p>
+					<p>$100</p>
+				</div>
+				<div>
+					<img src={placeholder} />
+				</div>
+			</div>
+			<button>CHECKOUT</button>
 		</div>
 	);
 }
