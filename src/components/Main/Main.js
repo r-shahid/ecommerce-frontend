e9dd1250b088e48e4b8045f470139c57741ea2a0
Riana <rianashahid@gmail.com>
2020-10-26 15:11:48 -0400

    added other routes, will add to nav bar now

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,31 +1,32 @@
-import React from 'react'
-import './Main.css'
-import Products from '../Products/Products'
-import Product from '../Product/Product'
-import Seller from '../Seller/Seller'
-import AboutUs from '../AboutUs/AboutUs'
-import Cart from '../Cart/Cart'
-import { Router, Switch, Route, Link } from 'react-router-dom'
+import React from 'react';
+import './Main.css';
+import Products from '../Products/Products';
+import Product from '../Product/Product';
+import Seller from '../Seller/Seller';
+import AboutUs from '../AboutUs/AboutUs';
+import Cart from '../Cart/Cart';
+import { Router, Switch, Route, Link } from 'react-router-dom';
 const Main = (props) => {
-    return (
-        <Switch>
-            <main>
-                <Route path='/products'>
-                    <Products />
-                </Route>
-                <Route path='/products:id'>
-                    <Product />
-                </Route>
-                
-                <Seller />
-                <Cart />
-                <AboutUs />
-            </main>
-        </Switch>
-            
-        
-       
-    )
-}
-export default Main
-
+	return (
+		<Switch>
+			<main>
+				<Route path='/'>
+					<Products />
+				</Route>
+				<Route path='/products:id'>
+					<Product />
+				</Route>
+				<Route path='/sell'>
+					<Seller />
+				</Route>
+				<Route path='/cart'>
+					<Cart />
+				</Route>
+				<Route path='/about'>
+					<AboutUs />
+				</Route>
+			</main>
+		</Switch>
+	);
+};
+export default Main;
