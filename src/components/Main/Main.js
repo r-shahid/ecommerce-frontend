6a31305ad0a3bb9b7b4bcecae53803a7aacdd20b
Tonimarie Morris <morris.tonimarie@gmail.com>
2020-10-26 11:29:51 -0400

    moved Cart to Main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,13 +4,14 @@ import Products from '../Products/Products'
 import Product from '../Product/Product'
 import Seller from '../Seller/Seller'
 import AboutUs from '../AboutUs/AboutUs'
-
+import Cart from '../Cart/Cart'
 const Main = (props) => {
     return (
         <main>
             <Products />
             <Product />
             <Seller />
+            <Cart />
             <AboutUs />
         </main>
        
