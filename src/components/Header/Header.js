839886288549ff30a7a17a05554e68978abff661
Riana <rianashahid@gmail.com>
2020-10-26 15:12:49 -0400

    navbar linked

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     return (
         <div className="header">
             <div className="logo">
-                <Link to="/products">
+                <Link to="/">
                     <p>LOGO</p>
                 </Link>
             </div>
