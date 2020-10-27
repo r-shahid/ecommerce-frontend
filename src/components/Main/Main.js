12312e4527d405de48ca992f11347404eedd389e
Riana <rianashahid@gmail.com>
2020-10-27 16:58:39 -0400

    added fetch in main - waiting on map in Products

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,64 +7,88 @@ import AboutUs from '../AboutUs/AboutUs';
 import Cart from '../Cart/Cart';
 import { Router, Switch, Route, Link } from 'react-router-dom';
 const Main = (props) => {
-	const [products, setProducts] = useState([])
-	
+	//url variable
+	const url = 'https://impulse-p3.herokuapp.com';
+	//state to hold products
+	const [products, setProducts] = useState([]);
+	//function to fetch all products
+	const getProducts = () => {
+		fetch(url + '/products/')
+			.then((response) => response.json())
+			.then((data) => {
+				setProducts(data);
+			});
+	};
+	//get products on page load, without creating an infinite loop
+	React.useEffect(() => {
+		getProducts();
+	}, []);
+
 	const emptyReview = {
 		name: '',
 		date: '',
-		rating: 0, 
-		review: ''
-	}
+		rating: 0,
+		review: '',
+	};
 	const emptyProduct = {
 		product: '',
 		img: '',
 		price: 0,
 		productDescription: '',
-		review: [
-			{emptyReview}
-		],
-	}
-	const handleUpdate = (review) => {
-		console.log('handleUpdate', handleUpdate)
-	}
-	const handleCreate = (product) => {
-		console.log('handleCreate', handleCreate)
-	}
+		review: [{ emptyReview }],
+	};
+	const handleCreate = (newProduct) => {
+		fetch(url + '/products/', {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(newProduct),
+		}).then(() => {
+			getProducts();
+		});
+	};
+	//handleUpdate to update a product when form is clicked
+	const handleUpdate = (product) => {
+		fetch(url + '/products/' + product._id, {
+			method: 'put',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(product),
+		}).then((response) => getProducts());
+	};
+
 	return (
 		<main>
 			<Switch>
 				<Route exact path='/'>
-					<Products />
+					<Products products={products} />
 				</Route>
-				<Route 
+				<Route
 					path='/products:id'
 					render={(rp) => (
-						<Product
+						<Product {...rp} handleSubmit={handleUpdate} review={emptyReview} />
+					)}
+				/>
+				<Route
+					path='/sell'
+					render={(rp) => (
+						<Seller
 							{...rp}
-							handleSubmit={handleUpdate}
-							review={emptyReview}
+							product={emptyProduct}
+							handleSubmit={handleCreate}
 						/>
 					)}
 				/>
-				<Route 
-					path='/sell'
-					render={(rp) => (
-						<Seller 
-							{...rp}
-							product={emptyProduct}
-							handleSubmit={handleCreate}	
-						/>
-					)}	
-				/>
-				<Route 
-					path='/cart'>
-						<Cart />
+				<Route path='/cart'>
+					<Cart />
 				</Route>
 				<Route path='/about'>
 					<AboutUs />
 				</Route>
 			</Switch>
-		</main>	
+		</main>
 	);
 };
 export default Main;
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -4,9 +4,11 @@ import placeholder from "../images/placeholder.jpg"
 //this page should have a map that creates multiple cards(maybe)?? 
 //which contain product images and names 
 const Products = (props) => {
+    const {products} = props
     return (
         <div>
             <img src={placeholder} alt='#'/>
+            {/* should be able to call product name by {products.name} */}
             <p>product name</p>
         </div>
       
