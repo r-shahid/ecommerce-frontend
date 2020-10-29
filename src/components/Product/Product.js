c6891fd72fa64f9dfa3fe9826a29df1594a29550
Tonimarie Morris <morris.tonimarie@gmail.com>
2020-10-29 04:48:07 -0400

    added comments to Product component to help with troubleshooting

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -5,12 +5,18 @@ import './Product.css'
 const Product = (props) => {
     console.log('product props- ', props)
     const [formData, setFormData] = React.useState(props.review)
+    //something is not quite right here, the filter is showing all products
+    //not just the one that was clicked
     const product = props.products.filter(product => product.name === props.match.params.name)
 
     console.log('result of product filter - ', product)
 	const handleSubmit = (e) => {
 	    e.preventDefault()
         props.handleSubmit(formData)
+        //have to figure out how to update this url to reflect the actual id
+        //of the selected item to post the review - this handleSubmit is for the
+        //review form submit 
+        //also should this update the item in the database? 
         props.history.push('/products/:id')
     }
     const handleChange = (e) => {
@@ -18,7 +24,10 @@ const Product = (props) => {
         e.preventDefault()
         setFormData({ ...formData, [e.target.name]: e.target.value })
     }
-
+    //nothing is actually being rendered here except a broken image link
+    //I suspect it has something to do with the filter not outputting just one
+    //product or maybe I've referenced it incorrectly here
+    //I'm kind of lost
     return (
         <div>
             <div>
