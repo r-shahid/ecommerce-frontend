1ddce4dc2ac59ebeb9c2cf7384edbbe0838a6d5f
Riana <rianashahid@gmail.com>
2020-10-29 16:51:28 -0400

    desktop styling done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,45 +6,61 @@ const Cart = (props) => {
 	// console.log('props-Cart', props);
 	return (
 		<div className='cart-content'>
-			<h1>Cart</h1>
+			<h1>Your Cart</h1>
 			{/* later these items will be pulled from whatever is in the cart --> same way that favorites is handled */}
-			<div className='each-product'>
-				<div className='product-info'>
-					<p>PRODUCT NAME</p>
-					<p>$100</p>
+			<div className='cart-page'>
+				<div className='products'>
+					<div className='each-product'>
+					<div className='product-info'>
+						<p>PRODUCT NAME</p>
+						<p>$100</p>
+					</div>
+					<div>
+						<img src={placeholder} />
+					</div>
 				</div>
-				<div>
-					<img src={placeholder} />
+					<div className='each-product'>
+					<div className='product-info'>
+						<p>PRODUCT NAME</p>
+						<p>$100</p>
+					</div>
+					<div>
+						<img src={placeholder} />
+					</div>
 				</div>
+					<div className='each-product'>
+					<div className='product-info'>
+						<p>PRODUCT NAME</p>
+						<p>$100</p>
+					</div>
+					<div>
+						<img src={placeholder} />
+					</div>
+				</div>
+					<div className='each-product'>
+					<div className='product-info'>
+						<p>PRODUCT NAME</p>
+						<p>$100</p>
+					</div>
+					<div>
+						<img src={placeholder} />
+					</div>
+				</div>
+				</div>
+				<div className='vertical-line'></div>
+				<div className='checkout'>
+					<div className='bonus-info'>
+						<h4>Total</h4>
+						<p>total sum from the other column if possible, otherwise delete this p tag and the h4 above it</p>
+						<p>Thank you for shopping with us. Orders made before 5pm EST will be shipped out same day. Shipping will be calculated on the following page. Gift wrap options are available for most items for an additional fee. Multiple orders made on the same day to the same address will be shipped together by default. Please contact the seller for separate shipments. </p>
+						<input type='text' value='Enter Coupon Code' />
+					</div>
+					<button>CHECKOUT</button>
+					<p>IMPULSE is not responsible for fraudulent listings. If you receive an item that does not match its listing, please contact the seller or appropriate authorities. IMPULSE is not responsible for items lost in the mail. We encourage sellers to offer package tracking and encourage buyers to make sure this is included.</p>
+				</div>
+				
 			</div>
-			<div className='each-product'>
-				<div className='product-info'>
-					<p>PRODUCT NAME</p>
-					<p>$100</p>
-				</div>
-				<div>
-					<img src={placeholder} />
-				</div>
-			</div>
-			<div className='each-product'>
-				<div className='product-info'>
-					<p>PRODUCT NAME</p>
-					<p>$100</p>
-				</div>
-				<div>
-					<img src={placeholder} />
-				</div>
-			</div>
-			<div className='each-product'>
-				<div className='product-info'>
-					<p>PRODUCT NAME</p>
-					<p>$100</p>
-				</div>
-				<div>
-					<img src={placeholder} />
-				</div>
-			</div>
-			<button>CHECKOUT</button>
+			
 		</div>
 	);
 }
--- src/components/Seller/Seller.js
+++ src/components/Seller/Seller.js
@@ -1,65 +1,85 @@
-import React from 'react'
-import './Seller.css'
-import headerimg from '../images/seller_header.png'
+import React from 'react';
+import './Seller.css';
+import headerimg from '../images/seller_header.png';
+import altheaderimg from '../images/alt_seller_header.png';
+import sidebar from '../images/sidebar.png';
 
 const Seller = (props) => {
-    console.log('seller-props', props)
-     const [formData, setFormData] = 
-     React.useState({
-         name: '',
-         img: '',
-         price: null,
-         description: ''
-     })
+	console.log('seller-props', props);
+	const [formData, setFormData] = React.useState({
+		name: '',
+		img: '',
+		price: null,
+		description: '',
+	});
 
 	//  const handleSubmit = (e) => {
 	//  	e.preventDefault()
-    //      props.handleSubmit(formData)
-    //      props.history.push('/')
-    //  }
-     const handleChange = (e) => {
-         console.log('handleChange', formData)
-         setFormData({ ...formData, [e.target.name]: e.target.value })
-    }
-    return (
-			<div className='seller'>
-				<img src={headerimg} alt='header' />
-				<h2>SELL ON IMPULSE</h2>
-				<form onSubmit={props.handleSubmit}>
-					<input
-						type='text'
-						placeholder='ITEM NAME'
-						name='name'
-						value={formData.name}
-						onChange={handleChange}
-					/>
-					<input
-						type='text'
-						placeholder='IMAGE URL'
-						name='img'
-						value={formData.img}
-						onChange={handleChange}
-					/>
-					<input
-						type='number'
-						placeholder='PRICE'
-						name='price'
-						value={formData.price}
-						onChange={handleChange}
-					/>
-					<textarea
-						rows='5'
-						type='text'
-						placeholder='ITEM DESCRIPTION'
-						name='description'
-						value={formData.description}
-						onChange={handleChange}
-					/>
-					<input className='submit' type='submit' value='SELL' />
-				</form>
-				<br />
-				<p className='terms-conditions'>By clicking "Sell" you confirm that you have read the IMPULSE Terms and Conditions and Privacy Policy. IMPULSE will take a variable percentage of all profits, depending on item popularity and seller success. Sellers may not direct customers to an external website for future sales. Sellers may not provide coupon codes as an incentive for positive reviews. Sellers caught engaging in these acts will not be able to post new items for sale for a length of time, depending on severity of their crimes. Sellers with repeated offenses will have past profit payments revoked and will be blacklisted from all eCommerce sites. </p>
+	//      props.handleSubmit(formData)
+	//      props.history.push('/')
+	//  }
+	const handleChange = (e) => {
+		console.log('handleChange', formData);
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
+	return (
+		<div className='seller'>
+			<img src={headerimg} alt='header' className='header-img' />
+			<img src={altheaderimg} alt='altheader' className='alt-header' />
+			<h2>SELL ON IMPULSE</h2>
+			<div className='form-and-sidebar'>
+				<div className='form-and-info'>
+					<form onSubmit={props.handleSubmit}>
+						<input
+							type='text'
+							placeholder='ITEM NAME'
+							name='name'
+							value={formData.name}
+							onChange={handleChange}
+						/>
+						<input
+							type='text'
+							placeholder='IMAGE URL'
+							name='img'
+							value={formData.img}
+							onChange={handleChange}
+						/>
+						<input
+							type='number'
+							placeholder='PRICE'
+							name='price'
+							value={formData.price}
+							onChange={handleChange}
+						/>
+						<textarea
+							rows='5'
+							type='text'
+							placeholder='ITEM DESCRIPTION'
+							name='description'
+							value={formData.description}
+							onChange={handleChange}
+						/>
+						<input className='submit' type='submit' value='SELL' />
+					</form>
+					<br />
+					<p className='terms-conditions'>
+						By clicking "Sell" you confirm that you have read the IMPULSE Terms
+						and Conditions and Privacy Policy. IMPULSE will take a variable
+						percentage of all profits, depending on item popularity and seller
+						success. Sellers may not direct customers to an external website for
+						future sales. Sellers may not provide coupon codes as an incentive
+						for positive reviews. Sellers caught engaging in these acts will not
+						be able to post new items for sale for a length of time, depending
+						on severity of their crimes. Sellers with repeated offenses will
+						have past profit payments revoked and will be blacklisted from all
+						eCommerce sites.{' '}
+					</p>
+				</div>
+				<div className='sidebar'>
+					<img src={sidebar} alt='sidebar' />
+				</div>
 			</div>
-		);
-}
-export default Seller
+		</div>
+	);
+};
+export default Seller;
